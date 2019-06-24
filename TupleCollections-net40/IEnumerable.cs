1ca03001622b1fba76800b7d95b525b8b0c66c62
Ofer Berkovitch <ofer.berkovitch@outlook.com>
2019-06-24 13:47:55 +0300

    Add IEnumrable

---
+++ TupleCollections-net40/IEnumerable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace TupleCollections
+{
+	public interface IEnumerable<T1, T2> : IEnumerable<Tuple<T1, T2>>
+	{
+	}
+
+	public interface IEnumerable<T1, T2, T3> : IEnumerable<Tuple<T1, T2, T3>>
+	{
+	}
+
+	public interface IEnumerable<T1, T2, T3, T4> : IEnumerable<Tuple<T1, T2, T3, T4>>
+	{
+	}
+
+	public interface IEnumerable<T1, T2, T3, T4, T5> : IEnumerable<Tuple<T1, T2, T3, T4, T5>>
+	{
+	}
+
+	public interface IEnumerable<T1, T2, T3, T4, T5, T6> : IEnumerable<Tuple<T1, T2, T3, T4, T5, T6>>
+	{
+	}
+
+	public interface IEnumerable<T1, T2, T3, T4, T5, T6, T7> : IEnumerable<Tuple<T1, T2, T3, T4, T5, T6, T7>>
+	{
+	}
+
+	public interface IEnumerable<T1, T2, T3, T4, T5, T6, T7, TRest> : IEnumerable<Tuple<T1, T2, T3, T4, T5, T6, T7, TRest>>
+	{
+	}
+}
