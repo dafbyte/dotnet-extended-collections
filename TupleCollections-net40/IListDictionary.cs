488f2c9ee4f04efb2fe54e8ba3b46dc0fb8b4537
Ofer Berkovitch <ofer.berkovitch@outlook.com>
2019-07-01 22:37:37 +0300

    Add IListDictionary

---
+++ TupleCollections-net40/IListDictionary.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+
+namespace TupleCollections
+{
+	public interface IListDictionary<TKey, TList, TItem> : ICollectionDictionary<TKey, TList, TItem>
+		where TList : IList<TItem>
+	{
+	}
+}
