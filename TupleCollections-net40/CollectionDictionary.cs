e461e3e3d828b2b84cdaf5e93a75d3d522fdc697
Ofer Berkovitch <ofer.berkovitch@outlook.com>
2019-07-01 22:38:19 +0300

    Add CollectionDictionary

---
+++ TupleCollections-net40/CollectionDictionary.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+
+namespace TupleCollections
+{
+	public class CollectionDictionary<TKey, TCollection, TItem> : Dictionary<TKey, TCollection>,
+		ICollectionDictionary<TKey, TCollection, TItem>
+		where TCollection : ICollection<TItem>, new()
+	{
+		public void Add(TKey key, TItem value)
+		{
+			if (!TryGetValue(key, out var collection) || collection == null)
+				this[key] = new TCollection { value };
+			else
+				collection.Add(value);
+		}
+	}
+}
