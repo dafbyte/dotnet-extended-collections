7ba0a1f19e933b23148d41019527f3e0f46164ab
Ofer Berkovitch <ofer.berkovitch@outlook.com>
2019-07-01 22:37:00 +0300

    Add ICollectionDictionary

---
+++ TupleCollections-net40/ICollectionDictionary.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+
+namespace TupleCollections
+{
+	public interface ICollectionDictionary<TKey, TCollection, TItem> : IDictionary<TKey, TCollection>
+		where TCollection : ICollection<TItem>
+	{
+		void Add(TKey key, TItem value);
+	}
+}
